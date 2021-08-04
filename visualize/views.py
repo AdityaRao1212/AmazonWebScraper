e76c246393efa13dd31d4fa89d4790f3a2a05f5f
StarboY09 <88194578+StarboY09@users.noreply.github.com>
2021-08-04 14:39:09 +0530

    Add files via upload

--- visualize/views.py
+++ visualize/views.py
@@ -1,3 +1,18 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+class VisualView(View):
+    def get(self, request):
+        return render(request, 'visual.html', {})
+
+    def post(self, request):
+        pass
+
+def get_data(request):
+    data = {
+        'sales': 100,
+        'customer': 10,
+    }
+    return JsonResponse(data)
